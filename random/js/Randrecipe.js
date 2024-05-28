91ef554fa14cf72589a20662476700617f0ba504
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-05-27 21:36:43 -0700

    rand-2

--- random/js/Randrecipe.js
+++ random/js/Randrecipe.js
@@ -1,3 +1,9 @@
+
+recipeList = ["garlic-bread", "cookies-n-cream"]
+
+
 function generateRandomRecipe(){
-    
-}
+    var RR = recipeList[(Math.floor(Math.random() * recipeList.length))]
+    return RR
+};
+console.log(generateRandomRecipe)
