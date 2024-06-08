a4085962ac0b23862cf606ab20e0b4ad096fdc48
gjjaureg <146881856+gjjaureg@users.noreply.github.com>
2024-06-08 16:05:11 -0700

    delete uneccessary

--- random/js/Randrecipe.js
+++
@@ -1,9 +0,0 @@
-
-recipeList = ["garlic-bread", "cookies-n-cream"]
-
-
-function generateRandomRecipe(){
-    var RR = recipeList[(Math.floor(Math.random() * recipeList.length))]
-    return RR
-};
-console.log(generateRandomRecipe)
